a3de2bc24294c7f6148c0a0e62bc7cafad0e0684
Anonymous <Anonymous>
2022-01-15 14:41:54 -0500

    Pre-render audio

--- Program.cs
+++ Program.cs
@@ -18,6 +18,8 @@ namespace Poems
     {
         static void Main(string[] args)
         {
+            string rootUrl = "https://raw.githubusercontent.com/culturing/Poems/main/";
+
             var md = new Markdown();
 
             List<Poem> poems = new List<Poem>();
@@ -27,11 +29,17 @@ namespace Poems
             string indexHtml = string.Empty;
             foreach (var file in Directory.EnumerateFiles("Poems"))
             {
+                string filename = Path.GetFileNameWithoutExtension(file);
                 string content = File.ReadAllText(file);
                 string contentHtml = md.Transform(content);
+
+                if (File.Exists($"Audio/{filename}.m4a"))
+                {
+                    contentHtml += $"<audio controls src='{rootUrl}/Audio/{filename}.m4a' style='width:100%'></audio>";
+                }
+
                 string finalPoemHtml = poemTemplate.Replace("{{poem}}", contentHtml);
 
-                string filename = Path.GetFileNameWithoutExtension(file);
                 string finalPath = $"Pages/{filename}.html";
                 File.WriteAllText(finalPath, finalPoemHtml);
 
