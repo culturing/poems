f3edb4b97a02d2f555e03ac7b5cd351d2703a6f7
culturing <80988180+culturing@users.noreply.github.com>
2022-12-21 16:56:31 -0500

    Replace IronPdf with Playwright and PdfSharp. Reorganize

--- Program.cs
+++ Program.cs
@@ -3,9 +3,12 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text.RegularExpressions;
+using System.Threading.Tasks;
 using HeyRed.MarkdownSharp;
-using IronPdf;
-using IronPdf.Bookmarks;
+using Microsoft.Playwright;
+using PdfSharp.Pdf;
+using PdfSharp.Pdf.IO;
+using PdfSharp.Drawing;
 
 namespace Poems
 {
@@ -25,11 +28,12 @@ namespace Poems
         static string ContentTemplate = File.ReadAllText("Templates/content.html");
         static List<Poem> Poems { get; set; } = new List<Poem>();
         static Dictionary<string, List<Poem>> PoemsByDate = new Dictionary<string, List<Poem>>();
-        static string RootUrl = "https://raw.githubusercontent.com/culturing/Poems/main/";
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {            
-            Directory.Delete("Pages", true);
-            Directory.CreateDirectory("Pages");
+            Directory.Delete("Output", true);
+            Directory.CreateDirectory("Output/Poems");  
+            Directory.CreateDirectory("Output/Pdfs");  
+            Directory.CreateDirectory("Output/Other");  
         
         // Parse Poems
             foreach (string dirpath in Directory.EnumerateDirectories("Poems"))
@@ -66,29 +70,11 @@ namespace Poems
 
             File.WriteAllText("index.html", finalIndexHtml);
 
-            RenderOtherPage("Other/Why Poetry.md");
-            RenderOtherPage("Other/Favorite Poems.md");
             RenderOtherPage("Other/FAQ.md");
+            RenderOtherPage("Other/Favorite Poems.md");
+            RenderOtherPage("Other/Why Poetry.md");
 
-        // Render Pdf
-            // ChromePdfRenderer renderer = GetPdfRenderer();
-            // PdfDocument pdf = renderer.RenderHtmlFileAsPdf("index.html");
-            // pdf.Bookmarks.AddBookMarkAtStart("Contents", 0);
-
-            // IPdfBookMark currentMonthBookmark = null;
-            // foreach(KeyValuePair<string, List<Poem>> kvp in PoemsByMonth)
-            // {
-            //     currentMonthBookmark = pdf.Bookmarks.AddBookMarkAtEnd(kvp.Key, pdf.PageCount);
-            //     foreach(Poem poem in kvp.Value)
-            //     {
-            //         PdfDocument poemPdf = renderer.RenderHtmlFileAsPdf(poem.FilePath);
-            //         string bookmarkName = Path.GetFileNameWithoutExtension(poem.FilePath);
-            //         currentMonthBookmark.Children.AddBookMarkAtEnd(bookmarkName, pdf.PageCount);
-            //         pdf.AppendPdf(poemPdf);            
-            //     }
-            // }
-    
-            // pdf.SaveAs("Poems.pdf");
+            await RenderPdf("Poems.pdf");
         }
 
         static void AddPoem(string filepath)
@@ -115,15 +101,8 @@ namespace Poems
             
             string content = String.Join("  \n", lines);
             string contentHtml = md.Transform(content);
-
-            // if (File.Exists($"Audio/{filename}.m4a"))
-            // {
-            //     contentHtml += $"<audio controls src='{rootUrl}/Audio/{filename}.m4a' style='width:100%'></audio>";
-            // }
-
             string finalPoemHtml = ContentTemplate.Replace("{{content}}", contentHtml);
-
-            string finalPath = $"Pages/{filename}.html";
+            string finalPath = $"Output/Poems/{filename}.html";
             File.WriteAllText(finalPath, finalPoemHtml);
 
             poem.Link = $"<a href=\"{finalPath}\">{filename}</a>";
@@ -131,6 +110,7 @@ namespace Poems
 
             Poems.Add(poem);
 
+        // Sort for chronology
             string key = poem.PublicationDate.ToString("yyyy-MM-dd");
             if (!PoemsByDate.ContainsKey(key))
                 PoemsByDate[key] = new List<Poem>();
@@ -141,19 +121,64 @@ namespace Poems
         {
             string text = File.ReadAllText(filepath);
             string html = ContentTemplate.Replace("{{content}}", md.Transform(text));
-            string htmlpath = filepath.Replace(Path.GetExtension(filepath), ".html");
+            string htmlpath = $"Output/Other/{Path.GetFileNameWithoutExtension(filepath)}.html";
             File.WriteAllText(htmlpath, html);   
         }
 
-        static ChromePdfRenderer GetPdfRenderer()
+        static async Task RenderPdf(string outpath)
         {
-            ChromePdfRenderer renderer = new ChromePdfRenderer();
-            renderer.RenderingOptions.CssMediaType = IronPdf.Rendering.PdfCssMediaType.Print;
-            renderer.RenderingOptions.MarginTop = 12;
-            renderer.RenderingOptions.MarginBottom = 12;
-            renderer.RenderingOptions.MarginRight = 12;
-            renderer.RenderingOptions.MarginLeft = 12;
-            return renderer;
+            System.Text.Encoding.RegisterProvider(System.Text.CodePagesEncodingProvider.Instance);
+
+            using IPlaywright playwright = await Playwright.CreateAsync();
+            await using IBrowser browser = await playwright.Chromium.LaunchAsync();
+            IPage page = await browser.NewPageAsync();
+
+            using (PdfDocument pdf = new PdfDocument())
+            {
+                XFont font = new XFont("Quattrocento", 12.0);
+                XBrush brush = XBrushes.Black;
+
+                pdf.AddPage(new PdfPage());
+                PdfOutline poemsOutline = pdf.Outlines.Add("Poems", pdf.Pages[0]);
+                foreach(KeyValuePair<string, List<Poem>> kvp in PoemsByDate.OrderByDescending(kvp => kvp.Key))
+                {
+                    Directory.CreateDirectory($"Output/Pdfs/{kvp.Key}");
+                    foreach(Poem poem in kvp.Value)
+                    {
+                        string pdfpath = $"Output/Pdfs/{kvp.Key}/{Path.GetFileNameWithoutExtension(poem.FilePath)}.pdf";
+                        await page.GotoAsync("file:///" + Path.GetFullPath(poem.FilePath));
+                        PagePdfOptions options = new PagePdfOptions 
+                        { 
+                            Path = pdfpath, 
+                            Margin = new Margin { Left = "1in", Top = "1in", Right = "1in", Bottom = "1in" },
+                        };
+                        await page.PdfAsync(options);
+
+                        using (PdfDocument poemPdf = PdfReader.Open(pdfpath, PdfDocumentOpenMode.Import))
+                        {
+                            foreach (PdfPage pdfPage in poemPdf.Pages)
+                            {
+                                PdfPage addedPage = pdf.AddPage(pdfPage);
+                            
+                            // Add page number
+                                using (XGraphics gfx = XGraphics.FromPdfPage(addedPage))
+                                {
+                                    double x = 0;
+                                    double y = addedPage.Height - font.Height - new XUnit(0.5, XGraphicsUnit.Inch);
+                                    double width = addedPage.Width - new XUnit(0.5, XGraphicsUnit.Inch);
+                                    double height = font.Height;
+                                    gfx.DrawString($"{pdf.PageCount}", font, brush, new XRect(x, y, width, height), XStringFormats.CenterRight);
+                                }
+                            }
+                            poemsOutline.Outlines.Add(poem.Title, pdf.Pages[pdf.PageCount - poemPdf.PageCount]);
+                        }
+                    }
+                }
+                PdfPage indexPage = pdf.AddPage();
+                pdf.Outlines.Add("Index", pdf.Pages[pdf.PageCount - 1]);
+
+                pdf.Save(outpath);
+            }
         }
     }
 }
